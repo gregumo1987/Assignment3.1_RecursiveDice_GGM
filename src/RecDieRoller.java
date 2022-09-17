c158a17d8e6c39388630b95c061385da60427bae
Greg Gutierrez <108035989+gregumo1987@users.noreply.github.com>
2022-09-17 17:42:08 -0400

    first commit

---
+++ src/RecDieRoller.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+/**
+ * A die roller class with roll method that
+ * continues rolling until a "2" is rolled.
+ * 
+ * @author Greg Gutierrez
+ **/
+
+public class RecDieRoller {
+	/** rollCount property tracks the number of times this die has been rolled
+	 * in the current effort to roll a "2". */
+	private int rollCount;
+	/** printFlag property can be set to control whether the result of each 
+	 * roll is printed. */
+	private boolean printFlag;
+	
+	/** Empty argument constructor. */
+	public RecDieRoller() {
+		this.rollCount = 0;
+		this.printFlag = true;
+	}
+	
+	/** Getter for rollCount property.
+	 * @return the rollCount */
+	public int getRollCount() {
+		return rollCount;
+	}// end getRollCount
+
+	/** Setter for rollCount property.
+	 * @param rollCount the rollCount to set */
+	public void setRollCount(int rollCount) {
+		this.rollCount = rollCount;
+	}// end setRollCount
+	
+	/** Getter for printFlag property.
+	 * @return the printFlag */
+	public boolean isPrintFlag() {
+		return printFlag;
+	}// end isPrintFlag
+
+	/** Setter for printFlag property.
+	 * @param printFlag the printFlag to set */
+	public void setPrintFlag(boolean printFlag) {
+		this.printFlag = printFlag;
+	}// end setPrintFlag
+	
+	/** Method to roll the die until the result is a "2".
+	 * Stores the number of rolls in the rollCount property
+	 * of the die. */
+	public void roll() {
+		Random random = new Random();
+		int newRoll = random.nextInt(6) + 1;
+		if (printFlag)
+			System.out.format("%-18sRolled a \"%1d\"\n", " ", newRoll);
+		rollCount++;
+		// Check for base case.
+		if (newRoll == 2) {
+			return;
+		} else {
+			roll(); // Recursive call.
+		}
+	}// end roll
+
+}// end class
---
+++ src/RecDieRollerDriver.java
@@ -0,0 +1,42 @@
+import java.text.DecimalFormat;
+/**
+ * A driver class that rolls a "2" four times and displays the results.
+ * Then it rolls "2" 1000 times and reports the average number of rolls 
+ * required.
+ * 
+ * @author Greg Gutierrez
+ **/
+
+public class RecDieRollerDriver {
+	/** Main class */
+	public static void main(String[] args) {
+		RecDieRoller recRoller = new RecDieRoller();
+		DecimalFormat df=new DecimalFormat("#.00");
+		int totalRollCount = 0;
+		
+		System.out.println("\n       ATTEMPTS TO ROLL A \"2\" ");
+		System.out.println("      ------------------------");
+		for (int i=1; i<=4; i++) {
+			System.out.println("\n      Attempt #" + i + ":");
+			// Reset rollCount property for each new attempt to roll a "2".
+			recRoller.setRollCount(0); 
+			recRoller.roll();
+		}
+		
+		// Set printFlag to false during the 1000 attempts to roll a "2".
+		recRoller.setPrintFlag(false);
+		for (int i=1; i<=1000; i++) {
+			// Reset rollCount property for each new attempt to roll a "2".
+			recRoller.setRollCount(0);
+			recRoller.roll();
+			totalRollCount+= recRoller.getRollCount();
+		}
+		double averageNumberRolls = (double)totalRollCount/1000;
+		String anr = df.format(averageNumberRolls);
+		
+		System.out.println("\n  After 1000 attempts, the average number of "
+				+ "rolls to get a \"2\" was " + anr);
+		
+	}// end main
+
+}// end class
